19cb0852ca487f533efdd17951fb1952df39c74a
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2022-10-23 22:12:16 +0200

    homepage update

--- album/views.py
+++ album/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .models import Photo, Category
 
 
 # Create your views here.
 def album(request):
-    return render(request, 'album/album.html')
+    photos = Photo.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'photos': photos,
+        'categories': categories,
+    }
+    return render(request, 'album/album.html', context)
 
 
 def photoView(request, pk):
+    photo = Photo.objects.get(id=pk)
     return render(request, 'album/photo.html')
 
 
