273bc93295bad0d1213f059b7b32fab455f83f0a
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2022-10-23 22:03:56 +0200

    saving photos

--- album/models.py
+++ album/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
 class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.CharField(max_length=64, null=True, blank=True)
-    image = models.ImageField(upload_to='photos', null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.description
