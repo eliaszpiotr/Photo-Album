020ee351d3affea2abab0921a79d1e719c9fdebb
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2022-10-23 20:44:14 +0200

    projest setup

--- photo_album/settings.py
+++ photo_album/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'album.app.AlbumConfig',
+    'album.apps.AlbumConfig',
 ]
 
 MIDDLEWARE = [
