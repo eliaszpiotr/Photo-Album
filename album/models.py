e25d24ba400a36031e4185e0774b640eba988528
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2022-10-23 21:49:00 +0200

    models, migrations

--- album/models.py
+++ album/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.CharField(max_length=64, null=True, blank=True)
+    image = models.ImageField(upload_to='photos', null=False, blank=False)
+
+    def __str__(self):
+        return self.description
